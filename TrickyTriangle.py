2be2c6011a9c0c0c6bf1d16b176f14d7e8a8e560
damasjo1 <john.damask@novartis.com>
2018-07-23 20:34:57 -0400

    housekeeping

--- TrickyTriangle.py
+++ TrickyTriangle.py
@@ -4,8 +4,10 @@ import enum
 
 logging.basicConfig(level=logging.INFO)
 
+
 def log_it(message):
     logging.debug(message)
+
 
 # All possible ways a peg can move on the board
 class Directions(enum.Enum):
@@ -45,33 +47,15 @@ class Board:
                     counter += 1
             self.map.append(row)
 
-    # Add pegs to all but one spot in the board
-    # TODO This tightly couples holes and pegs. Not sure that's the best approach
-    # Maybe it's ok...get rid of the peg concept and see moves as positional requests
-    def add_pegs(self, empty_position=None):
-        # Set a random board position as empty
-        empty = -1
-        if empty_position is None:
-            empty = random.randint(0, self.size - 1)
-        else:
-            if empty_position > self.size:
-                raise Exception('fixed_position variable greater than board size. Try again')
-            empty = empty_position
-        log_it("Empty positions: " + str(empty))
-        for row in self.map:
-            for hole in row:
-                position = hole[0]
-                hole.append(0) if position == empty else hole.append(1) # PEG = 1; EMPTY = 0
-
     # For a given triangle hole, determine possible moves
     def check_direction(self, item):
         index, row, column = item
         directions = []
-        if (column-2) >= 0:
+        if (column - 2) >= 0:
             directions.append(Directions.LEFT)
         if (column + 2) <= row:
             directions.append(Directions.RIGHT)
-        if row >= 2 and (column -2) >= 0:
+        if row >= 2 and (column - 2) >= 0:
             directions.append(Directions.UP_LEFT)
         if row >= 2 and (column + 2) <= row:
             directions.append(Directions.UP_RIGHT)
@@ -80,19 +64,30 @@ class Board:
             directions.append(Directions.DOWN_RIGHT)
         return directions
 
+    # Add pegs to all but one spot in the board
+    # TODO This tightly couples holes and pegs. Not sure that's the best approach
+    # Maybe it's ok...get rid of the peg concept and see moves as positional requests
+    def add_pegs(self, empty_position=None):
+        if empty_position is None:
+            empty = random.randint(0, self.size - 1)  # Set a random board position as empty
+        else:
+            if empty_position > self.size:
+                raise Exception('fixed_position variable greater than board size. Try again')
+            empty = empty_position
+        log_it("Empty positions: " + str(empty))
+        for row in self.map:
+            for hole in row:
+                position = hole[0]
+                hole.append(0) if position == empty else hole.append(1)  # PEG = 1; EMPTY = 0
+
     def dump_map(self):
         for i in self.map:
             print(*i)  # Pythonic syntax to unpack lists
 
 
-# Returns a row / column string for printing
-def RC(r, c):
-    return "R" + str(r) + "C" + str(c)
-
-
 b = Board(3)
 b.add_pegs()
-#b.dump_map()
+# b.dump_map()
 # Stores individual moves. Originating positions are stored in the odd elements and destinations are in even
 # A full game will in in a list of size (b.size - 1)*2
 moves = []
