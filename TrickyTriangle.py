c354598a748a8a9009b9885599fbd633397655c7
damasjo1 <john.damask@novartis.com>
2018-07-24 11:51:48 -0400

    Starting to create move logic

--- TrickyTriangle.py
+++ TrickyTriangle.py
@@ -24,6 +24,9 @@ class Board:
     def __init__(self, side_length):
         self.side_length = side_length
         self.size = int((side_length * (side_length + 1)) / 2)
+        # To have one peg left on a board starting with one empty position,
+        # the number of possible moves is 1 - number of pegs
+        self.max_moves = self.size - 2
         self.map = []
         self.empty_positions = []
         self.create_board_map()
@@ -122,8 +125,14 @@ class Board:
                 moves.append(destination_hole)
         return moves
 
-    def choose_move(self):
-        pass
+    def choose_move(self, move_options):
+        """Contains logic for selecting a game move
+
+        :param move_options: Dictionary of possible moves for a given game state
+        :return selection: Dictionary of selected move to make
+        """
+        selection = random.choice(list(move_options))
+        return selection
 
     def dump_map(self):
         for i in self.map:
@@ -132,17 +141,27 @@ class Board:
 
 b = Board(5)
 b.add_pegs()
-b.dump_map()
+#b.dump_map()
 # Stores individual moves. Originating positions are stored in the odd elements and destinations are in even
 # A full game will in in a list of size (b.size - 1)*2
-moves = []
+game_moves = []
 win_ledger = []  # Stores a list of games (i.e. as many moves lists that end with one peg left as are computed)
 
 # Gameplay
-for hole in b.flat_map:
-    if hole[4]: # If hole has peg, let's see if it can move
-        moves = b.determine_game_moves(hole)
-       # print ("Peg at position " + str(hole[0]) + " has " + str(len(moves)) + " possible moves")
-        for m in moves:
-            print("Peg in position " + str(hole[0]) + " can move to position " + str(m))
+#for i in range(0, b.max_moves):
+for i in range(0, 1):
+    moves = {}
+    for hole in b.flat_map: # Determine all possible board moves
+        if hole[4]: # If hole has peg, let's see where it can move
+            m = b.determine_game_moves(hole)
+            for mv in m:
+                moves[hole[0]] = mv
 
+                # for m in moves:
+        #     print("Peg in position " + str(hole[0]) + " can move to position " + str(m))
+
+    print(moves)
+    the_chosen = b.choose_move(moves)
+    c = {the_chosen: moves[the_chosen]}
+    print(c)
+    i += 1
